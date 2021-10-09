c6a021b15bff173c942384333d05d7018dda85bc
r-teren <romanteren1990@gmail.com>
2021-10-09 10:57:28 +0300

    primitive logging added

--- src/test/java/selenium/search/tests/testSelen.java
+++ src/test/java/selenium/search/tests/testSelen.java
@@ -4,15 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import selenium.search.webpages.resultsPage;
 import selenium.search.webpages.searchPage;
 import java.util.concurrent.TimeUnit;
 
+
 public class testSelen {
     WebDriver driver;
+    private static final Logger log = LogManager.getLogger(testSelen.class.getName());
+
 
     @Before
     public void setup(){
@@ -21,22 +25,31 @@ public class testSelen {
         System.setProperty("webdriver.chrome.driver", projectPath+"\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-               }
+        //URL
+        String searchURL = "https://google.com";
+        driver.get(searchURL);
+        log.info("Google search page opened;");
+
+
+    }
 
     @Test
     public void TestPages () {
 
-       searchPage googleSearch = new searchPage(driver);
-       Assert.assertTrue(googleSearch.isTitleOk());
-       googleSearch.searchSelenium();
+        searchPage googleSearch = new searchPage(driver);
+        Assert.assertTrue(googleSearch.isTitleOk());
+        log.info("Title Checked.Its Google;");
+        googleSearch.searchSelenium();
+        log.info("Searching for Selenium;");
 
         resultsPage selCount =new resultsPage(driver);
         selCount.countSelenium();
-        }
+        log.info("Results printed;");
+    }
 
-   @After
+    @After
     public void close() {
         driver.close();
+        log.info("Browser closed;");
     }
 }
-
--- src/test/java/selenium/search/webpages/resultsPage.java
+++ src/test/java/selenium/search/webpages/resultsPage.java
@@ -2,6 +2,7 @@ package selenium.search.webpages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -9,17 +10,21 @@ import java.util.List;
 public class resultsPage {
     private WebDriver driver;
 
-    By containSelenium=By.xpath("//*[contains (text(),'Selenium')]");
+    @FindBy(xpath="//*[contains (text(),'Selenium')]")
+    private List<WebElement> containSelenium;
+    //By containSelenium=By.xpath("//*[contains (text(),'Selenium')]");
 
     public resultsPage(WebDriver driver) {
-       this.driver=driver;
-       WebDriverWait loadWait = new WebDriverWait(driver,3);
-               }
+        this.driver=driver;
+        WebDriverWait loadWait = new WebDriverWait(driver,3);
+    }
 
-     public void countSelenium () {
-            List<WebElement> countSelenium = driver.findElements(containSelenium);
-        }
-        }
+    public void countSelenium () {
+        System.out.println(containSelenium);
+        //List<WebElement> countSelenium = driver.findElements(containSelenium);
+
+    }
+}
 
 
 
